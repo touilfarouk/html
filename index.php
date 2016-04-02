41403b940cce818d57d2f5f3edbf159fe37d09df
Farouk Touil <farouktouil@hotmail.com>
2016-04-02 11:00:29 +0100

    initialize a html repository

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>tire</title>
+</head>
+<body>
+<head>gohead</head>
+
+
+
+ phpunit -c phpunit.xml --coverage-html coverage
+
+ sudo chmod 777 vendor
+
+
+</body>
+</html>
