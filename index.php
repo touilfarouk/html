edb0dfe1f7671f528b80358bea52626dab761a6e
farouktouil <farouktouil@hotmail.com>
2018-02-09 12:20:57 +0100

    we want to test this

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 
 <?php use \Foo\Bar\Bar;
 require dirname(__DIR__) . '/html/autoload.php';
+//test this
 $bar = new Bar();
 $bar->sayHello();
-//git push origin master  ..... 
+//git push origin master  ..... .......
+// testing the sages
